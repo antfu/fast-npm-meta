c3463294c466cd884d36d70133bd4102ea764ef1
Anthony Fu <github@antfu.me>
2024-07-02 15:23:12 +0200

    fix: improve `pickRegistry` type

--- package/src/helpers.ts
+++ package/src/helpers.ts
@@ -3,22 +3,25 @@ export const NPM_REGISTRY = 'https://registry.npmjs.org/'
 /**
  * Lightweight replacement of `npm-registry-fetch` function `pickRegistry`'
  *
- * @param scope - npm scope
+ * @param scope - scope of package, get from 'npm-package-arg'
  * @param npmConfigs - npm configs, read from `.npmrc` file
+ * @param defaultRegistry - default registry, default to 'https://registry.npmjs.org/'
  */
 export function pickRegistry(
-  scope: string,
-  npmConfigs: Record<string, string>,
+  scope: string | null | undefined,
+  npmConfigs: Record<string, unknown>,
   defaultRegistry = NPM_REGISTRY,
 ): string {
-  let registry = scope && npmConfigs[`${scope.replace(/^@?/, '@')}:registry`]
+  let registry: string | undefined = scope
+    ? npmConfigs[`${scope.replace(/^@?/, '@')}:registry`] as string
+    : undefined
 
-  if (!registry && npmConfigs.scope) {
-    registry = npmConfigs[`${npmConfigs.scope.replace(/^@?/, '@')}:registry`]
+  if (!registry && typeof npmConfigs.scope === 'string') {
+    registry = npmConfigs[`${npmConfigs.scope.replace(/^@?/, '@')}:registry`] as string
   }
 
   if (!registry) {
-    registry = npmConfigs.registry || defaultRegistry
+    registry = npmConfigs.registry as string || defaultRegistry
   }
 
   return registry
